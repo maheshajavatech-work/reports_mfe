eb2ba93a2c8431ff3e29ac78aeb9700954184f6a
maheshajavatec-work <maheshajavatech@gmail.com>
2025-04-15 09:58:11 -0400

    Initial commit: Reports MFE extracted from monorepo

---
+++ src/app/report/report.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { ReportComponent } from './report.component';
+
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: ReportComponent
+  }
+];
